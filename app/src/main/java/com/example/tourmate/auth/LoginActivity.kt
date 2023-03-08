bcdb1784a113ee5045909a4ebfc06fc419884ff1
vinhcotam <66359687+vinhcotam@users.noreply.github.com>
2023-03-08 17:29:44 +0700

    first commit

---
+++ app/src/main/java/com/example/tourmate/auth/LoginActivity.kt
@@ -0,0 +1,66 @@
+package com.example.tourmate.auth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tourmate.MainActivity
+import com.example.tourmate.R
+import com.example.tourmate.databinding.ActivityLoginBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+    private lateinit var auth: FirebaseAuth
+
+    //    private lateinit var googleSignInClient : GoogleSignInClient
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { result: FirebaseAuthUIAuthenticationResult ->
+
+        if (result.resultCode == RESULT_OK) {
+            //success
+            openMainActivity()
+        } else {
+            //failed
+        }
+    }
+    private val listAuthProviders = listOf(
+        AuthUI.IdpConfig.GoogleBuilder().build(),
+        AuthUI.IdpConfig.EmailBuilder().build(),
+        AuthUI.IdpConfig.PhoneBuilder().build(),
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+
+        binding.buttonLogin.setOnClickListener {
+            startSignIn()
+        }
+        if (auth.currentUser != null) {
+            openMainActivity()
+        }
+    }
+
+    private fun openMainActivity() {
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun startSignIn() {
+        val signInIntent = AuthUI.getInstance().createSignInIntentBuilder()
+            .setIsSmartLockEnabled(false)
+            .setAvailableProviders(listAuthProviders)
+            .setLogo(R.drawable.welcome)
+//            .setTheme(R.style.ThemeLoginUI)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+}
